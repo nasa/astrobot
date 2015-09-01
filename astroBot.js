a00038d63f5062da59cbb692c4a00abc825a16ea
William Baker <william.baker@tietronix.com>
2015-09-01 11:33:37 -0500

    test

--- astroBot.js
+++ astroBot.js
@@ -5,7 +5,7 @@ var http = require("http");
 
 setInterval(function() {
     http.get("http://ancient-shore-4819.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
+}, 300000); // every 5 minutes(300000)
 
 // Unique token for the AstroBot
 var token = 'xoxb-8952060609-DKLOV3KXRS3aynsZG9WEhsPz';
